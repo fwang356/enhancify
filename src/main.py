d173548f35c12665e571e8d5e756fdd8bd4b24fb
Felix Wang <fwang356@gatech.edu>
2021-07-07 21:03:10 -0500

    Added recommendation system

--- src/main.py
+++ src/main.py
@@ -20,45 +20,94 @@ token = util.prompt_for_user_token(username, scope)
 time_range = sys.argv[1]
 
 
-def get_top_tracks(spotify, limit):
-    results = spotify.current_user_top_tracks(limit=limit, offset=0, time_range=time_range)
+# Returns list of user's top ten track id's
+def get_top_tracks():
+    results = sp.current_user_top_tracks(limit=10, offset=0, time_range=time_range)
     tracks = []
-    for song in range(limit):
+    for song in range(10):
         tracks.append(results["items"][song]["id"])
     return tracks
 
 
-def average_mood(spotify):
-    tracks = get_top_tracks(spotify, 50)
-    tracks_mood = sp.audio_features(tracks)
+# Returns list of moods for inputted tracks
+def get_mood(tracks):
+    track_analysis = sp.audio_features(tracks)
+    moods = {}
+    for i in range(len(tracks)):
+        tracks_mood = {tracks[i]: {'danceability': track_analysis[i]['danceability'],
+                                   'energy': track_analysis[i]['energy'], 'valence': track_analysis[i]['valence'],
+                                   'tempo': track_analysis[i]['tempo']}}
+        moods.update(tracks_mood)
+    return moods
+
+
+# Returns average mood for user's top tracks
+def average_mood():
+    tracks = get_top_tracks()
+    track_analysis = sp.audio_features(tracks)
     danceability = 0
     energy = 0
     valence = 0
     tempo = 0
 
-    for track in tracks_mood:
+    for track in track_analysis:
         danceability = danceability + track['danceability']
         energy = energy + track['energy']
         valence = valence + track['valence']
         tempo = tempo + track['tempo']
 
-    danceability = round(danceability / 50, 5)
-    energy = round(energy / 50, 5)
-    valence = round(valence / 50, 5)
-    tempo = round(tempo / 50, 5)
+    danceability = round(danceability / 10, 5)
+    energy = round(energy / 10, 5)
+    valence = round(valence / 10, 5)
+    tempo = round(tempo / 10, 5)
 
     average = {'danceability': danceability, 'energy': energy, 'valence': valence, 'tempo': tempo}
     return average
 
 
+# Returns list of recommended tracks based on inputted track
+def recommendation(track):
+    track_analysis = sp.audio_features(track)
+
+    danceability = track_analysis[0]['danceability']
+    energy = track_analysis[0]['energy']
+    valence = track_analysis[0]['valence']
+    tempo = track_analysis[0]['tempo']
+
+    artist_id = sp.track(track)['artists'][0]['id']
+    artists = [artist_id]
+    tracks = [track]
+    genre = sp.artist(artist_id)['genres']
+
+    recommended = sp.recommendations(seed_artists=artists, seed_genres=genre, seed_tracks=tracks, limit=10,
+                                     target_danceability=danceability, target_energy=energy,
+                                     target_valence=valence, target_tempo=tempo)
+    rec_tracks = []
+
+    for i in range(10):
+        rec_tracks.append(recommended['tracks'][i]['id'])
+
+    while track in rec_tracks:
+        rec_tracks.remove(track)
+        recommended = sp.recommendations(seed_artists=artists, seed_genres=genre, seed_tracks=tracks, limit=1,
+                                         target_danceability=danceability, target_energy=energy,
+                                         target_valence=valence, target_tempo=tempo)
+        rec_tracks.append(recommended['tracks'][0]['id'])
+
+    return rec_tracks
+
+
 if token:
     sp = spotipy.Spotify(auth=token)
-    top_tracks = get_top_tracks(sp, 10)  # List of user's top ten tracks
-    tracks_moods = sp.audio_features(top_tracks)  # List of moods for user's top tracks
-    average_mood = average_mood(sp)  # User's average mood from top fifty tracks
+    top_tracks = get_top_tracks()
+    tracks_analysis = get_mood(top_tracks)
+    average_mood = average_mood()
     if len(sys.argv) > 2:
         track_id = sys.argv[2]
-        mood = sp.audio_features(track_id)  # Mood for inputted track
+        mood = get_mood([track_id])
+    recommendations = recommendation(top_tracks[0])
+    rec_analysis = get_mood(recommendations)
+    print(top_tracks)
+    print(rec_analysis)
 else:
     print("Can't get token for " + username)
-

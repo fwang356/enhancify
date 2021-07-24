3faccc5602645562ee633b1a5435d36da161a1f0
Felix Wang <fwang356@gatech.edu>
2021-07-24 18:11:22 -0500

    commented out if search query is empty

--- app.py
+++ app.py
@@ -172,9 +172,11 @@ def search():
     sp = spotipy.Spotify(auth=session.get('token_info').get('access_token'))
     print(len(request.form['search']))
     print(request.form['search'])
+    """
     if len(request.form['search']) == 0:
         print('empty')
         return ('', 204)
+    """
     query = request.form['search']
     results = search_track(query, sp)
     return render_template('search.html', sp=sp, results=results, query=query)
